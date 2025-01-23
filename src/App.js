367db02e573324dfec0323084cd81b73654716da
MatheusAG16 <103548678+MatheusAG16@users.noreply.github.com>
2025-01-23 15:56:16 -0300

    Update App.js
    Alteração do link "/" para "/home", para ser direcionado primeiramente à pagina home.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './components/pages/Home'
-import Contato from './components/pages/Contato'
-import Company from './components/pages/Company'
-import NewProject from './components/pages/NewProject'
-import Project from './components/pages/Project'
-import Projects from './components/pages/Projects'
-import Container from './components/layout/Container'
-import NavBar from './components/layout/NavBar'
-import Footer from './components/layout/Footer'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contato from "./components/pages/Contato";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Project from "./components/pages/Project";
+import Projects from "./components/pages/Projects";
+import Container from "./components/layout/Container";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
@@ -15,17 +15,17 @@ function App() {
       <NavBar />
       <Container customClass="min-height">
         <Routes>
-            <Route exact path="/" element={<Home />} />
+          <Route exact path="/home" element={<Home />} />
 
-            <Route path="/contato" element={<Contato />} />
+          <Route path="/contato" element={<Contato />} />
 
-            <Route path="/projects" element={<Projects />} />
+          <Route path="/projects" element={<Projects />} />
 
-            <Route path="/company" element={<Company />} />
+          <Route path="/company" element={<Company />} />
 
-            <Route path="/newproject" element={<NewProject />} />
+          <Route path="/newproject" element={<NewProject />} />
 
-            <Route path="project/:id" element={<Project />} />
+          <Route path="project/:id" element={<Project />} />
         </Routes>
       </Container>
       <Footer />
